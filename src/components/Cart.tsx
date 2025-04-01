778c9288cff992745c8bf2fe317714c6d48cb9a5
Jacob <jacobseated@gmail.com>
2025-04-01 22:33:42 +0200

    add empty card messages, include fonts

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,19 +2,20 @@
 
 import React, { useEffect, useRef } from 'react'
 import { useCart } from '@/context/CartContext'
+import EmptyCart from '@/components/EmptyCart'
 import ChaoticLogo from '@/assets/chaotic-beverage-research-logo.svg'
-import { headFont, openSans } from '@/lib/fonts';
+import { headFont, openSans } from '@/lib/fonts'
 
 const Cart = () => {
-  const { isCartOpen, toggleCart } = useCart() // Access cart state
+  // Access cart state in isCartOpen, cartItems contains the individual objects
+  const { isCartOpen, toggleCart, cartItems } = useCart()
 
   const cartRef = useRef<HTMLDivElement | null>(null)
-
   useEffect(() => {
     if (isCartOpen) {
       document.addEventListener('mousedown', (event: MouseEvent) => {
         // If someone clicks outside the cart, close it
-        console.log(event.target)
+        // console.log(event.target)
         if (
           cartRef.current &&
           !cartRef.current.contains(event.target as Node)
@@ -42,7 +43,6 @@ const Cart = () => {
 
   // Don't render if cart is closed
   if (!isCartOpen) return null
-
   return (
     <div className='fixed w-screen h-screen z-2 top-0 left-0 flex items-center justify-center'>
       <div
@@ -56,9 +56,29 @@ const Cart = () => {
         </div>
         <div className='inline-flex items-center justify-center w-full'>
           <hr className='w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700' />
-          <h2 className={`${headFont.className} absolute px-3 text-2xl text-gray-900 -translate-x-1/2 bg-amber-300 left-1/2 rounded-sm`}>Cart</h2>
+          <h2
+            className={`${headFont.className} absolute px-3 text-2xl text-gray-900 -translate-x-1/2 bg-amber-300 left-1/2 rounded-sm`}
+          >
+            Cart
+          </h2>
         </div>
-        
+        <EmptyCart />
+        <ul className='space-y-2'>
+          {cartItems.map((item, index) => (
+            <li
+              key={index}
+              className='border-b border-gray-300 pb-1 grid grid-flow-row-dense grid-cols-2'
+            >
+              <div className='col-end-2'>
+                <div className='font-semibold'>{item.strDrink}</div>
+                <div className='text-sm text-gray-700'>
+                  Quantity: <b>{item.quantity}</b>
+                </div>
+              </div>
+              <div className='text-right'>2000 kr</div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
