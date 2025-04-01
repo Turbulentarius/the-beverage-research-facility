d3996b98dfad46d896e339b12431d809fda50e69
Jacob <jacobseated@gmail.com>
2025-04-01 22:35:20 +0200

    hacking with tailwind responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { CartProvider } from '@/context/CartContext'
 import ChaoticLogo from '@/assets/chaotic-beverage-research-logo.svg'
 import CartButton from '@/components/CartButton'
+import CartNotifications from '@/components/CartNotifications'
 import Cart from '@/components/Cart';
 import './globals.css'
 import { headFont, openSans } from '@/lib/fonts';
@@ -21,27 +22,28 @@ export default function RootLayout ({
       <body className={`${openSans.className} antialiased`}>
         <CartProvider>
           <div className='w-[90%] mx-auto'>
-            <header className='w-full max-w-[70rem] min-w-[400px] mx-auto flex items-end h-40 my-4 p-4 bg-amber-300 rounded-sm text-gray-950'>
-              <div className='siteLogo h-full w-40'>
+            <header className='w-full max-w-[70rem] min-w-[320px] h-56 sm:h-40 mx-auto sm:flex sm:items-end my-4 p-4 bg-amber-300 rounded-sm text-gray-950'>
+              <div className='siteLogo h-[70%] lg:h-full float-left sm:float-none'>
                 <ChaoticLogo />
               </div>
-              <div>
+              <div className='pb-4 float-left sm:float-none'>
                 <div
-                  className={`${headFont.className} text-2xl 2xl:text-4xl px-4`}
+                  className={`${headFont.className} text-xl lg:text-4xl px-4`}
                 >
                   {metadata.title}
                 </div>
-                <div className='px-4'>{metadata.description}</div>
+                <div className='sm:px-4'>{metadata.description}</div>
               </div>
-              <div>
+              <div className='sm:ml-auto clear-both'>
                 <CartButton />
               </div>
             </header>
-            <main className='w-full max-w-[70rem] min-w-[400px] mx-auto'>
+            <main className='w-full max-w-[70rem] min-w-[320px] mx-auto'>
               {children}
             </main>
           </div>
           <Cart />
+          <CartNotifications />
         </CartProvider>
       </body>
     </html>
