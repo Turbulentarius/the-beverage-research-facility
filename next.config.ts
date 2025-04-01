0834f10d89c6a506756d12ff5c5b94175e5d9b40
Jacob <jacobseated@gmail.com>
2025-04-01 13:40:34 +0200

    add svg handler from svgr docs

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,50 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    // Source: https://react-svgr.com/docs/next/
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule: { test: { test: (arg0: string) => any } }) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'www.thecocktaildb.com',
-      },
-    ],
+        hostname: 'www.thecocktaildb.com'
+      }
+    ]
   },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: false
   },
-};
+  devIndicators: false
+}
 
-export default nextConfig;
+export default nextConfig
