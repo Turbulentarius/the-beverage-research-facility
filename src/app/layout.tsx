d646eb078fb9f401a7e5310640b12ccbc20aa073
Jacob <jacobseated@gmail.com>
2025-04-01 13:42:32 +0200

    added CartProvider, Logo, Fonts + more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,49 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
-import { Open_Sans } from 'next/font/google'
-
-const openSans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'],
-  variable: '--font-open-sans',
-})
+import type { Metadata } from 'next'
+import { CartProvider } from '@/context/CartContext'
+import ChaoticLogo from '@/assets/chaotic-beverage-research-logo.svg'
+import CartButton from '@/components/CartButton'
+import Cart from '@/components/Cart';
+import './globals.css'
+import { headFont, openSans } from '@/lib/fonts';
 
 export const metadata: Metadata & { title: string; description: string } = {
-  title: "The Chaotic Beverage Research Facility",
-  description: "We don't make drinks; we summon them.",
-};
+  title: 'The Chaotic Beverage Research Facility',
+  description: "We don't make drinks; we summon them."
+}
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${openSans.variable} antialiased`}
-      >
-        <header className='w-screen max-w-[70rem] mx-auto'>
-          <div>
-          {metadata.title}
+    <html lang='en'>
+      <body className={`${openSans.className} antialiased`}>
+        <CartProvider>
+          <div className='w-[90%] mx-auto'>
+            <header className='w-full max-w-[70rem] min-w-[400px] mx-auto flex items-end h-40 my-4 p-4 bg-amber-300 rounded-sm text-gray-950'>
+              <div className='siteLogo h-full w-40'>
+                <ChaoticLogo />
+              </div>
+              <div>
+                <div
+                  className={`${headFont.className} text-2xl 2xl:text-4xl px-4`}
+                >
+                  {metadata.title}
+                </div>
+                <div className='px-4'>{metadata.description}</div>
+              </div>
+              <div>
+                <CartButton />
+              </div>
+            </header>
+            <main className='w-full max-w-[70rem] min-w-[400px] mx-auto'>
+              {children}
+            </main>
           </div>
-        </header>
-        <main className='w-screen max-w-[70rem] mx-auto'>
-          {children}
-        </main>
+          <Cart />
+        </CartProvider>
       </body>
     </html>
-  );
+  )
 }
