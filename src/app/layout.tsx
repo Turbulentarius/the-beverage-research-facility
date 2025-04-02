5ace2e11a04cf6cf404f4de1367c0f5f71a2f120
Jacob <jacobseated@gmail.com>
2025-04-02 03:53:39 +0200

    responsiveness improved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,19 +22,19 @@ export default function RootLayout ({
       <body className={`${openSans.className} antialiased`}>
         <CartProvider>
           <div className='w-[90%] mx-auto'>
-            <header className='w-full max-w-[70rem] min-w-[320px] h-56 sm:h-40 mx-auto sm:flex sm:items-end my-4 p-4 bg-amber-300 rounded-sm text-gray-950'>
-              <div className='siteLogo h-[70%] lg:h-full float-left sm:float-none'>
+            <header className='w-full max-w-[70rem] min-w-[320px] h-auto sm:h-40 mx-auto sm:flex sm:items-end my-4 p-4 bg-amber-300 rounded-sm text-gray-950'>
+              <div className='siteLogo h-32 lg:h-full sm:float-none'>
                 <ChaoticLogo />
               </div>
-              <div className='pb-4 float-left sm:float-none'>
+              <div className='pb-4 sm:float-none'>
                 <div
                   className={`${headFont.className} text-xl lg:text-4xl px-4`}
                 >
                   {metadata.title}
                 </div>
-                <div className='sm:px-4'>{metadata.description}</div>
+                <div className='px-4'>{metadata.description}</div>
               </div>
-              <div className='sm:ml-auto clear-both'>
+              <div className='sm:ml-auto float-right'>
                 <CartButton />
               </div>
             </header>
