57d489f517fb8b73e74e93d11c1d3fdbacafa464
Jacob <jacobseated@gmail.com>
2025-04-02 06:01:52 +0200

    improve notification. add price to BuyButton.

--- src/components/BuyButton.tsx
+++ src/components/BuyButton.tsx
@@ -1,36 +1,56 @@
 'use client'
 
 import { useCart } from '@/context/CartContext'
-
-interface Drink {
-  idDrink: string
-  strDrink: string
-  price: number
-  quantity: number
-}
+import { Drink } from '@/lib/types/Drink'
 
 export default function BuyButton ({ drink }: { drink: Drink }) {
   const { addToCart, toggleCart, addNotification } = useCart()
 
+  // Add missing price. It's also missing on the API
+  // so I just add this placeholder price to have something to test with
+  drink.price = {
+    currency: 'USD',
+    gross: 0, // Including VAT
+    net: priceFromId(drink.idDrink), // Excluding VAT
+    vat: 25
+  }
+  // calculate gross (With VAT)
+  drink.price.gross = drink.price.net * (drink.price.vat / 100 + 1)
+
   return (
-    <button
-      type='button'
-      className='text-white bg-green-500 hover:bg-green-800 mb-4 cursor-pointer font-medium rounded-sm text-sm px-4 py-2 text-center inline-flex items-center me-2'
-      onClick={() => {
-        addToCart(drink)
-        addNotification({idDrink: drink.idDrink, quantity: 1, msg: 'Cart update: ' + drink.strDrink, timestamp: new Date()})
-      }}
-    >
-      <svg
-        className='w-3.5 h-3.5 me-2'
-        aria-hidden='true'
-        xmlns='http://www.w3.org/2000/svg'
-        fill='currentColor'
-        viewBox='0 0 18 21'
+    <>
+      <div className='text-green-500'>
+        {drink.price.gross} {drink.price.currency}
+      </div>
+      <button
+        type='button'
+        className='text-white bg-green-500 hover:bg-green-800 my-2 cursor-pointer font-medium rounded-sm text-sm px-4 py-1 text-center inline-flex items-center me-2'
+        onClick={() => {
+          addToCart(drink)
+          addNotification({
+            idDrink: drink.idDrink,
+            quantity: 1,
+            msg: '+1 ' + ' ' + drink.strDrink,
+            timestamp: new Date()
+          })
+        }}
       >
-        <path d='M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z' />
-      </svg>
-      Buy
-    </button>
+        <svg
+          className='w-3.5 h-3.5 me-2'
+          aria-hidden='true'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='currentColor'
+          viewBox='0 0 18 21'
+        >
+          <path d='M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z' />
+        </svg>
+        Buy
+      </button>
+    </>
   )
+}
+
+function priceFromId(id: string): number {
+  const hash = [...id].reduce((acc, char) => acc + char.charCodeAt(0), 0)
+  return parseFloat(((hash % 12) + 10).toFixed(2)) // Between 10.00 and 21.99
 }
