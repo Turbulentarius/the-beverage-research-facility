ff5796d41d24f93c9106a21a9f4107debe6e49ba
Jacob <jacobseated@gmail.com>
2025-04-02 06:02:24 +0200

    improved notification

--- src/components/CartNotifications.tsx
+++ src/components/CartNotifications.tsx
@@ -16,7 +16,7 @@ export default function CartNotifications() {
   
     const timer = setTimeout(() => {
       setVisibleNotifications([])
-    }, 1500)
+    }, 2500)
   
     return () => clearTimeout(timer)
   }, [cartNotifications])
@@ -29,13 +29,9 @@ export default function CartNotifications() {
           key={n.idDrink}
           className='bg-green-100 text-gray-800 border border-green-300 px-4 py-2 rounded shadow'
         >
-          <div className='font-medium'>{n.msg} ({n.quantity})</div>
+          <div className='font-medium'>{n.msg}</div>
           <div className='text-xs text-gray-600 pt-2'>
-            {n.timestamp.toLocaleTimeString('en-GB', {
-              hour: '2-digit',
-              minute: '2-digit',
-              second: '2-digit',
-            })}
+            ({n.quantity} in cart)
           </div>
         </div>
       ))}
