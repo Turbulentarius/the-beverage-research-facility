6cf97ccf289671110b22cfe8cd0886dcc0a09f89
Jacob <jacobseated@gmail.com>
2025-04-03 17:12:52 +0200

    Changed to uuid for notifications

--- src/components/CartNotifications.tsx
+++ src/components/CartNotifications.tsx
@@ -9,7 +9,7 @@ export default function CartNotifications () {
     <div className='fixed top-4 right-4 z-50 space-y-2'>
       {cartNotifications.map(n => (
         <div
-          key={`${n.idDrink}-${n.timestamp}`}
+          key={`${n.notificationId}`}
           className='bg-green-100 text-gray-800 border border-green-300 px-4 py-2 rounded shadow'
         >
           <div className='font-medium'>{n.msg}</div>
