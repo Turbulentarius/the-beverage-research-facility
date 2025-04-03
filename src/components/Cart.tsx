4a7d69c5d1dd1441904543641a67b49dd59278dc
Jacob <jacobseated@gmail.com>
2025-04-03 17:17:12 +0200

    add ClearCart button

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -8,7 +8,7 @@ import { headFont, openSans } from '@/lib/fonts'
 
 const Cart = () => {
   // Access cart state in isCartOpen, cartItems contains the individual objects
-  const { isCartOpen, toggleCart, cartItems } = useCart()
+  const { isCartOpen, toggleCart, clearCart, cartItems } = useCart()
 
   const cartRef = useRef<HTMLDivElement | null>(null)
   useEffect(() => {
@@ -69,7 +69,7 @@ const Cart = () => {
         className='bg-white shadow-lg px-2 z-3 py-1 w-full max-w-[70rem] h-full rounded-sm'
       >
         <div className='h-28'>
-          <div className='siteLogo h-full w-40 mx-auto'>
+          <div className='siteLogo h-full w-fit mx-auto'>
             <ChaoticLogo />
           </div>
         </div>
@@ -107,12 +107,37 @@ const Cart = () => {
           ))}
         </ul>
         <div className='w-56 grid grid-flow-row text-red-500'>
-          <h2 className='text-gray-950 text-xl mt-2 bg-amber-300 rounded-sm p-2'>Total:</h2>
+          <h2 className='text-gray-950 text-xl mt-2 bg-amber-300 rounded-sm p-2'>
+            Total:
+          </h2>
           <div>
             {grossTotal} {currency}
           </div>
           <div className='text-gray-950 text-sm'>
             {netTotal} {currency} without VAT ({vat}%)
+          </div>
+          <div className="pt-4">
+            <button
+              type='button'
+              className='text-white bg-black hover:bg-gray-800 mb-4 cursor-pointer font-medium rounded-sm text-sm px-2 py-2 text-center inline-flex items-center me-2'
+              onClick={clearCart}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='1.5'
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
+                />
+              </svg>
+              Clear cart
+            </button>
           </div>
         </div>
       </div>
